0b088cd91c7400bf456cacfbc3add694cce0eee8
me <mothership2002@naver.com>
2024-11-27 15:35:35 +0900

    이벤트소싱 추가 중

--- be/Post/src/main/java/com/example/webflux/common/aop/EventSourceAspect.java
+++ be/Post/src/main/java/com/example/webflux/common/aop/EventSourceAspect.java
@@ -9,28 +9,44 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 @Component
 @Aspect
 @Slf4j(topic = "EventSource")
 @RequiredArgsConstructor
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class EventSourceAspect {
 
     private final SimpleEventPublisher publisher;
 
+    // 중요
     // 어노테이션을 매개 변수로 잡을 시 바인딩 아규먼츠 익셉션이 발생하여 리플렉션으로 핸들링.
     @Around("@annotation(com.example.webflux.common.annotation.EventPublishPoint)")
     public Object publishEvent(ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        EventPublishPoint annotation = method.getAnnotation(EventPublishPoint.class);
-        log.debug("Event published with value: {}, {}", joinPoint, annotation.value());
-        Object proceed = joinPoint.proceed();
-        publisher.publish(new EventWrapper(joinPoint.getTarget(), annotation.value()));
-        return proceed;
+        final String key = "start";
+        return Mono.deferContextual(context -> {
+            try {
+                MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+                Method method = signature.getMethod();
+                EventPublishPoint annotation = method.getAnnotation(EventPublishPoint.class);
+                return ((Mono<?>) joinPoint.proceed()).doOnSuccess(e -> {
+                    long start = context.get(key);
+                    EventWrapper event = new EventWrapper(joinPoint.getTarget(), annotation.value());
+                    log.info("[{}] Event Published, Type : [{}], processed time : [{}ms]",
+                            event.getSource().getClass().getSimpleName(),
+                            event.getType(),
+                            (System.nanoTime() - start) / 1000000);
+                    publisher.publish(event);
+                });
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }).contextWrite(context -> context.put(key, System.nanoTime()));
     }
 }
--- be/Post/src/main/java/com/example/webflux/common/event/SimpleEventPublisher.java
+++ be/Post/src/main/java/com/example/webflux/common/event/SimpleEventPublisher.java
@@ -1,19 +1,16 @@
 package com.example.webflux.common.event;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
-@Slf4j(topic = "EventPublisher")
 @RequiredArgsConstructor
 public class SimpleEventPublisher {
 
     private final ApplicationEventPublisher publisher;
 
     public void publish(CustomEvent event) {
-        log.info("{} Event Published : {}", event.getSource().getClass().getSimpleName(), event.getType());
         publisher.publishEvent(event);
     }
 
